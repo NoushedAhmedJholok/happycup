807419ad8cef86b4a2228995f30df31531330058
noushedahmedjholok <info.jholok@gmail.com>
2024-09-12 13:35:58 +0600

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,558 @@
+(function($) {
+
+	$(document).ready(function() {
+	  $('body').addClass('js');
+	  var $menu = $('#menu'),
+	    $menulink = $('.menu-link');
+	  
+	$menulink.click(function() {
+	  $menulink.toggleClass('active');
+	  $menu.toggleClass('active');
+	  return false;
+	});});
+
+
+	videoPopup();
+
+
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    nav:true,
+	    autoplay:true,
+		autoplayTimeout:5000,
+		autoplayHoverPause:true,
+	    responsive:{
+	        0:{
+	            items:1
+	        },
+	        550:{
+	            items:2
+	        },
+	        750:{
+	            items:3
+	        },
+	        1000:{
+	            items:4
+	        },
+	        1200:{
+	            items:5
+	        }
+	    }
+	})
+
+
+	$(".Modern-Slider").slick({
+	    autoplay:true,
+	    autoplaySpeed:10000,
+	    speed:600,
+	    slidesToShow:1,
+	    slidesToScroll:1,
+	    pauseOnHover:false,
+	    dots:true,
+	    pauseOnDotsHover:true,
+	    cssEase:'fade',
+	   // fade:true,
+	    draggable:false,
+	    prevArrow:'<button class="PrevArrow"></button>',
+	    nextArrow:'<button class="NextArrow"></button>', 
+	});
+
+
+	$("div.features-post").hover(
+	    function() {
+	        $(this).find("div.content-hide").slideToggle("medium");
+	    },
+	    function() {
+	        $(this).find("div.content-hide").slideToggle("medium");
+	    }
+	 );
+
+
+	$( "#tabs" ).tabs();
+
+
+	(function init() {
+	  function getTimeRemaining(endtime) {
+	    var t = Date.parse(endtime) - Date.parse(new Date());
+	    var seconds = Math.floor((t / 1000) % 60);
+	    var minutes = Math.floor((t / 1000 / 60) % 60);
+	    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+	    var days = Math.floor(t / (1000 * 60 * 60 * 24));
+	    return {
+	      'total': t,
+	      'days': days,
+	      'hours': hours,
+	      'minutes': minutes,
+	      'seconds': seconds
+	    };
+	  }
+	  
+	  function initializeClock(endtime){
+	  var timeinterval = setInterval(function(){
+	    var t = getTimeRemaining(endtime);
+	    document.querySelector(".days > .value").innerText=t.days;
+	    document.querySelector(".hours > .value").innerText=t.hours;
+	    document.querySelector(".minutes > .value").innerText=t.minutes;
+	    document.querySelector(".seconds > .value").innerText=t.seconds;
+	    if(t.total<=0){
+	      clearInterval(timeinterval);
+	    }
+	  },1000);
+	}
+	initializeClock(((new Date()).getFullYear()+1) + "/1/1")
+	})()
+
+})(jQuery);
+
+$('.slider_main_banner').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: false,
+	autoplaySpeed: 2000,
+	arrows: false,
+  });
+
+// Update the countdown every second
+const countdownInterval = setInterval(function() {
+	// Get the current date and time
+	const now = new Date().getTime();
+  
+	// Calculate the difference between now and the target date
+	const distance = targetDate - now;
+  
+	// Calculate the time components
+	const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+	// Display the result in the respective elements
+	document.getElementById("days").innerText = days;
+	document.getElementById("hours").innerText = hours;
+	document.getElementById("minutes").innerText = minutes;
+	document.getElementById("seconds").innerText = seconds;
+  
+	// If the countdown is over, clear the interval and display a message
+	if (distance < 0) {
+		clearInterval(countdownInterval);
+		document.getElementById("countdown").innerText = "EXPIRED";
+	}
+  }, 1000);
+
+  
+// Update the countdown every second
+const interval = setInterval(function() {
+    // Get the current date and time
+    const currentTime = new Date().getTime();
+
+    // Calculate the difference between now and the target date
+    const timeRemaining = endDate - currentTime;
+
+    // Calculate the time components
+    const daysLeft = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hoursLeft = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutesLeft = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    const secondsLeft = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    // Display the result in the respective elements
+    document.getElementById("dayCount").innerText = daysLeft;
+    document.getElementById("hourCount").innerText = hoursLeft;
+    document.getElementById("minuteCount").innerText = minutesLeft;
+    document.getElementById("secondCount").innerText = secondsLeft;
+
+    // If the countdown is over, clear the interval and display a message
+    if (timeRemaining < 0) {
+        clearInterval(interval);
+        document.getElementById("timer").innerText = "EXPIRED";
+    }
+}, 1000);
+
+const list = document.querySelectorAll(".nav__item");
+list.forEach((item) => {
+  item.addEventListener("click", () => {
+    list.forEach((item) => item.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+
+
+
+
+
+// Table 
+
+
+var box = paginator({
+    table: document.getElementById("DataTable").getElementsByTagName("table")[0],
+    box_mode: "list",
+});
+box.className = "box";
+document.getElementById("table_box_bootstrap").appendChild(box);
+
+
+/*****************************************************
+ * Paginator Function                                *
+ *****************************************************
+ * config : {
+ *     get_rows : function used to select rows to do pagination on
+ *         If no function is provided, checks for a config.table element and looks for rows in there to page
+ *
+ *     box : Empty element that will have page buttons added to it
+ *         If no config.box is provided, but a config.table is, then the page buttons will be added using the table
+ *
+ *     table : table element to be paginated
+ *         not required if a get_rows function is provided
+ *
+ *     rows_per_page : number of rows to display per page
+ *         default number is 10
+ *
+ *     page: page to display
+ *         default page is 1
+ *
+ *     box_mode: "list", "buttons", or function. determines how the page number buttons are built.
+ *         "list" builds the page index in list format and adds class "pagination" to the ul element. Meant for use with bootstrap
+ *         "buttons" builds the page index out of buttons
+ *         if this field is a function, it will be passed the config object as its ownly param and assumed to build the page index buttons
+ *
+ *     page_options: false or [{text: , value: }, ... ] used to set what the dropdown menu options are available, resets rows_per_page value
+ *         false prevents the options from being displayed
+ *         [{text: , value: }, ... ] allows you to customize what values can be chosen, a value of 0 will display all the table's rows.
+ *         the default setup is
+ *           [
+ *               { value: 5,  text: '5'   },
+ *               { value: 10, text: '10'  },
+ *               { value: 20, text: '20'  },
+ *               { value: 50, text: '50'  },
+ *               { value: 100,text: '100' },
+ *               { value: 0,  text: 'All' }
+ *           ]
+ *
+ *      active_class: set the class for page buttons to have when active.
+ *          defaults to "active"
+ *
+ *     tail_call: function to be called after paginator is done
+ * }
+ */
+function paginator(config) {
+    // throw errors if insufficient parameters were given
+    if (typeof config != "object")
+        throw "Paginator was expecting a config object!";
+    if (typeof config.get_rows != "function" && !(config.table instanceof Element))
+        throw "Paginator was expecting a table or get_row function!";
+
+    // get/make an element for storing the page numbers in
+    var box;
+    if (!(config.box instanceof Element)) {
+        config.box = document.createElement("div");
+    }
+    box = config.box;
+
+    // get/make function for getting table's rows
+    if (typeof config.get_rows != "function") {
+        config.get_rows = function () {
+            var table = config.table
+            var tbody = table.getElementsByTagName("tbody")[0]||table;
+
+            // get all the possible rows for paging
+            // exclude any rows that are just headers or empty
+            children = tbody.children;
+            var trs = [];
+            for (var i=0;i<children.length;i++) {
+                if (children[i].nodeType = "tr") {
+                    if (children[i].getElementsByTagName("td").length > 0) {
+                        trs.push(children[i]);
+                    }
+                }
+            }
+
+            return trs;
+        }
+    }
+    var get_rows = config.get_rows;
+    var trs = get_rows();
+
+    // get/set rows per page
+    if (typeof config.rows_per_page == "undefined") {
+        var selects = box.getElementsByTagName("select");
+        if (typeof selects != "undefined" && (selects.length > 0 && typeof selects[0].selectedIndex != "undefined")) {
+            config.rows_per_page = selects[0].options[selects[0].selectedIndex].value;
+        } else {
+            config.rows_per_page = 10;
+        }
+    }
+    var rows_per_page = config.rows_per_page;
+
+    // get/set current page
+    if (typeof config.page == "undefined") {
+        config.page = 1;
+    }
+    var page = config.page;
+
+    // get page count
+    var pages = (rows_per_page > 0)? Math.ceil(trs.length / rows_per_page):1;
+
+    // check that page and page count are sensible values
+    if (pages < 1) {
+        pages = 1;
+    }
+    if (page > pages) {
+        page = pages;
+    }
+    if (page < 1) {
+        page = 1;
+    }
+    config.page = page;
+ 
+    // hide rows not on current page and show the rows that are
+    for (var i=0;i<trs.length;i++) {
+        if (typeof trs[i]["data-display"] == "undefined") {
+            trs[i]["data-display"] = trs[i].style.display||"";
+        }
+        if (rows_per_page > 0) {
+            if (i < page*rows_per_page && i >= (page-1)*rows_per_page) {
+                trs[i].style.display = trs[i]["data-display"];
+            } else {
+                trs[i].style.display = "none";
+            }
+        } else {
+            trs[i].style.display = trs[i]["data-display"];
+        }
+    }
+
+    // page button maker functions
+    config.active_class = config.active_class||"active";
+    if (typeof config.box_mode != "function" && config.box_mode != "list" && config.box_mode != "buttons") {
+        config.box_mode = "button";
+    }
+    if (typeof config.box_mode == "function") {
+        config.box_mode(config);
+    } else {
+        var make_button;
+        if (config.box_mode == "list") {
+            make_button = function (symbol, index, config, disabled, active) {
+                var li = document.createElement("li");
+                var a  = document.createElement("a");
+                a.href = "#";
+                a.innerHTML = symbol;
+                a.addEventListener("click", function (event) {
+                    event.preventDefault();
+                    this.parentNode.click();
+                    return false;
+                }, false);
+                li.appendChild(a);
+
+                var classes = [];
+                if (disabled) {
+                    classes.push("disabled");
+                }
+                if (active) {
+                    classes.push(config.active_class);
+                }
+                li.className = classes.join(" ");
+                li.addEventListener("click", function () {
+                    if (this.className.split(" ").indexOf("disabled") == -1) {
+                        config.page = index;
+                        paginator(config);
+                    }
+                }, false);
+                return li;
+            }
+        } else {
+            make_button = function (symbol, index, config, disabled, active) {
+                var button = document.createElement("button");
+                button.innerHTML = symbol;
+                button.addEventListener("click", function (event) {
+                    event.preventDefault();
+                    if (this.disabled != true) {
+                        config.page = index;
+                        paginator(config);
+                    }
+                    return false;
+                }, false);
+                if (disabled) {
+                    button.disabled = true;
+                }
+                if (active) {
+                    button.className = config.active_class;
+                }
+                return button;
+            }
+        }
+
+        // make page button collection
+        var page_box = document.createElement(config.box_mode == "list"?"ul":"div");
+        if (config.box_mode == "list") {
+            page_box.className = "pagination";
+        }
+
+        var left = make_button("&laquo;", (page>1?page-1:1), config, (page == 1), false);
+        page_box.appendChild(left);
+
+        for (var i=1;i<=pages;i++) {
+            var li = make_button(i, i, config, false, (page == i));
+            page_box.appendChild(li);
+        }
+
+        var right = make_button("&raquo;", (pages>page?page+1:page), config, (page == pages), false);
+        page_box.appendChild(right);
+        if (box.childNodes.length) {
+            while (box.childNodes.length > 1) {
+                box.removeChild(box.childNodes[0]);
+            }
+            box.replaceChild(page_box, box.childNodes[0]);
+        } else {
+            box.appendChild(page_box);
+        }
+    }
+  
+  var dvRecords = document.createElement("div");
+  dvRecords.className = "dvrecords";
+  box.appendChild(dvRecords);
+
+    // make rows per page selector
+    if (!(typeof config.page_options == "boolean" && !config.page_options)) {
+        if (typeof config.page_options == "undefined") {
+            config.page_options = [
+                { value: 5,  text: '5'   },
+                { value: 10, text: '10'  },
+                { value: 20, text: '20'  },
+                { value: 50, text: '50'  },
+                { value: 100,text: '100' },
+                { value: 0,  text: 'All' }
+            ];
+        }
+        var options = config.page_options;
+        var select = document.createElement("select");
+        select.className = "records";
+        for (var i=0;i<options.length;i++) {
+            var o = document.createElement("option");
+            o.value = options[i].value;
+            o.text = options[i].text;
+            select.appendChild(o);
+        }
+        select.value = rows_per_page;
+        select.addEventListener("change", function () {
+            config.rows_per_page = this.value;
+            paginator(config);
+        }, false);
+        dvRecords.appendChild(select);
+    }
+
+    // status message
+    var stat = document.createElement("span");
+    stat.className = "stats";
+    stat.innerHTML = "On page " + page + " of " + pages
+        + ", showing rows " + (((page-1)*rows_per_page)+1)
+        + " to " + (trs.length<page*rows_per_page||rows_per_page==0?trs.length:page*rows_per_page)
+        + " of " + trs.length;
+    
+    dvRecords.appendChild(stat);
+
+    // run tail function
+    if (typeof config.tail_call == "function") {
+        config.tail_call(config);
+    }
+    return box;
+}
+
+
+
+jQuery.fn.sortElements = (function(){
+    
+    var sort = [].sort;
+    
+    return function(comparator, getSortable) {
+        
+        getSortable = getSortable || function(){return this;};
+        
+        var placements = this.map(function(){
+            
+            var sortElement = getSortable.call(this),
+                parentNode = sortElement.parentNode,
+                
+                // Since the element itself will change position, we have
+                // to have some way of storing it's original position in
+                // the DOM. The easiest way is to have a 'flag' node:
+                nextSibling = parentNode.insertBefore(
+                    document.createTextNode(''),
+                    sortElement.nextSibling
+                );
+            
+            return function() {
+                
+                if (parentNode === this) {
+                    throw new Error(
+                        "You can't sort elements if any one is a descendant of another."
+                    );
+                }
+                
+                // Insert before flag:
+                parentNode.insertBefore(this, nextSibling);
+                // Remove flag:
+                parentNode.removeChild(nextSibling);
+                
+            };
+            
+        });
+       
+        return sort.call(this, comparator).each(function(i){
+            placements[i].call(getSortable.call(this));
+        });
+        
+    };
+    
+})();
+
+    var table = $('table');
+    
+    $('th')
+        .wrapInner('<span title="sort this column"/>')
+        .each(function(){
+            
+            var th = $(this),
+                thIndex = th.index(),
+                inverse = false;
+            
+            th.click(function(){
+                
+                table.find('td').filter(function(){
+                    
+                    return $(this).index() === thIndex;
+                    
+                }).sortElements(function(a, b){
+                    
+                    return $.text([a]) > $.text([b]) ?
+                        inverse ? -1 : 1
+                        : inverse ? 1 : -1;
+                    
+                }, function(){
+                    
+                    // parentNode is the element we want to move
+                    return this.parentNode; 
+                    
+                });
+                
+                inverse = !inverse;
+                    
+            });
+                
+        });
+        document.addEventListener('DOMContentLoaded', function() {
+            // Select all <a> tags within the menu
+            const navLinks = document.querySelectorAll('#menu .main-menu a');
+        
+            navLinks.forEach(function(link) {
+                link.addEventListener('click', function(event) {
+                    const href = this.getAttribute('href');
+                    
+                    // Force navigation to the link's href, bypassing any other JS interference
+                    if (href && !href.startsWith('#')) {
+                        window.location.href = href;
+                    } else if (!href) {
+                        event.preventDefault();
+                    }
+                });
+            });
+        });
+        
